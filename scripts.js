d5f0c62d72d408a3f673db908743659177801d5c
Hoang Luu Hien <89684135+hoanglongryu@users.noreply.github.com>
2024-05-02 17:58:17 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,34 @@
+//load
+$(window).on('load', function() {
+    setTimeout(function() {
+        $('#loading').fadeOut('slow', function() {
+            $(this).remove();
+        });
+    }, 1500);
+});
+
+//button click
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+
 //adu
 function playSound() {
   var sound = document.getElementById("mySound");
    sound.play();
 }
+
+//fps
+var fps = document.getElementById("fps");
+var startTime = Date.now();
+var frame = 0;
+    function tick(){var time = Date.now();
+    frame++;
+        if(time - startTime > 1000){fps.innerHTML = (frame / ((time - startTime) / 1000)).toFixed(1);
+        startTime = time;frame = 0;
+    }window.requestAnimationFrame(tick);
+}tick();
 
 //music
 var audio = new Audio('audio/25.mp3');
@@ -34,42 +60,85 @@ function toggleAudio(event) {
       });
 
 //thong bao
-$(document).ready(function() {
-    // process bar
-    setTimeout(function() {
-        firstQuestion();
-        $('.spinner').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-    }, 600);
-})
+// $(document).ready(function() {
+//     // process bar
+//     setTimeout(function() {
+//         firstQuestion();
+//         $('.spinner').fadeOut();
+//         $('#preloader').delay(350).fadeOut('slow');
+//         $('body').delay(350).css({
+//             'overflow': 'visible'
+//         });
+//     }, 600);
+// })
 
-    const CONFIG = {
-    introTitle: 'Welcome',
-    introDesc: `Hãy truy cập bằng điện thoại di động(nếu có thể).`,
-    btnIntro: 'OK',
-    }
+//     const CONFIG = {
+//     introTitle: 'Welcome',
+//     introDesc: `Hãy truy cập bằng điện thoại di động(nếu có thể).`,
+//     btnIntro: 'OK',
+//     }
 
-function firstQuestion(){
-    $('.content').hide();
-    Swal.fire({
-        title: CONFIG.introTitle,
-        text: CONFIG.introDesc,
-        imageUrl: 'img/catcute.jpg',
-        imageWidth: 300,
-        imageHeight: 300,
-        background: '#fff url("img/252525.png")',
-        imageAlt: 'Custom image',
-        confirmButtonText: CONFIG.btnIntro,
-        customClass: {
-            confirmButton: 'confirm-button-class'
-        }
-    }).then(function(){
-        $('.content').show(200);
-    })
+// function firstQuestion(){
+//     $('.content').hide();
+//     Swal.fire({
+//         title: CONFIG.introTitle,
+//         text: CONFIG.introDesc,
+//         imageUrl: 'img/catcute.jpg',
+//         imageWidth: 300,
+//         imageHeight: 300,
+//         background: '#fff url("img/252525.png")',
+//         imageAlt: 'Custom image',
+//         confirmButtonText: CONFIG.btnIntro,
+//         customClass: {
+//             confirmButton: 'confirm-button-class'
+//         }
+//     }).then(function(){
+//         $('.content').show(200);
+//     })
+// }
+
+//thong bao new
+var modal = document.getElementById("myModal");
+var btn = document.getElementsByClassName("close")[0];
+modal.style.display = "block";
+btn.onclick = function() {
+    modal.children[0].style.animationName = "modalclose";
+    modal.children[0].style.animationDuration = "0.25s";
+    setTimeout(function(){ modal.style.display = "none"; }, 250);
 }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.children[0].style.animationName = "modalclose";
+        modal.children[0].style.animationDuration = "0.25s";
+        setTimeout(function(){ modal.style.display = "none"; }, 250);
+    }
+}
+// kich co thong bao
+window.addEventListener('resize', function() {
+    var imgElements = document.getElementsByTagName('modal-content2');
+    for(var i = 0; i < imgElements.length; i++) {
+        if(window.innerHeight > window.innerWidth){
+            // Màn hình dọc
+            imgElements[i].style.width = '75%';
+        } else {
+            // Màn hình ngang
+            imgElements[i].style.width = '25%';
+        }
+    }
+});
+// button close
+window.addEventListener('resize', function() {
+    var imgElements = document.getElementsByTagName('close2');
+    for(var i = 0; i < imgElements.length; i++) {
+        if(window.innerHeight > window.innerWidth){
+            // Màn hình dọc
+            imgElements[i].style.width = '40%';
+        } else {
+            // Màn hình ngang
+            imgElements[i].style.width = '20%';
+        }
+    }
+});
 
 // Anti right click anh Block ctrl+u
 document.oncontextmenu = new Function("return false");
